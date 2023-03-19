7252871743d16223451267f7c428645f364e1815
Jared <jaredlll08@gmail.com>
2023-03-20 01:19:53 +0200

    Port to 1.18.2

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,9 +1,18 @@
 #!/usr/bin/env groovy
 
+def docsOutDir = 'docsOut'
+def docsRepositoryUrl = 'git@github.com:CraftTweaker/CraftTweaker-Documentation.git'
+def gitSshCredentialsId = 'crt_git_ssh_key'
+def botUsername = 'crafttweakerbot'
+def botEmail = 'crafttweakerbot@gmail.com'
+
+def documentationDir = 'CrafttweakerDocumentation'
+def exportDirInRepo = 'docs_exported/1.18/iceandfire'
+
 pipeline {
     agent any
     tools {
-        jdk "jdk8u292-b10"
+        jdk "jdk-17.0.1"
     }
     stages {
         stage('Clean') {
@@ -42,14 +51,42 @@ pipeline {
 
                     echo 'Deploying to CurseForge'
                     sh './gradlew curseforge'
+
                 }
             }
         }
-    }
-    post {
-        always {
-            archive 'build/libs/**.jar'
-            archive 'changelog.md'
+
+        stage('Exporting Documentation') {
+            steps {
+                echo "Cloning Repository at Branch main"
+
+                dir(documentationDir) {
+                    git credentialsId: gitSshCredentialsId, url: docsRepositoryUrl, branch: "main", changelog: false
+                }
+
+
+                echo "Clearing existing Documentation export"
+                dir(documentationDir) {
+                    sh "rm --recursive --force ./$exportDirInRepo"
+                }
+
+                echo "Moving Generated Documentation to Local Clone"
+                sh "mkdir --parents ./$documentationDir/$exportDirInRepo"
+                sh "mv ./$docsOutDir/* ./$documentationDir/$exportDirInRepo/"
+
+
+                echo "Committing and Pushing to the repository"
+                dir(documentationDir) {
+                    sshagent([gitSshCredentialsId]) {
+                        sh "git config user.name $botUsername"
+                        sh "git config user.email $botEmail"
+                        sh 'git add -A'
+                        //Either nothing to commit, or we create a commit
+                        sh "git diff-index --quiet HEAD || git commit -m 'CI Doc export for iceandfiretweaker build ${env.BRANCH_NAME}-${env.BUILD_NUMBER}\n\nMatches git commit ${env.GIT_COMMIT}'"
+                        sh "git push origin main"
+                    }
+                }
+            }
         }
     }
 }
--- src/main/java/com/blamejared/iceandfiretweaker/handler/DragonForge.java
+++ src/main/java/com/blamejared/iceandfiretweaker/handler/DragonForge.java
@@ -1,63 +1,93 @@
 package com.blamejared.iceandfiretweaker.handler;
 
-import com.blamejared.crafttweaker.api.CraftTweakerAPI;
-import com.blamejared.crafttweaker.api.annotations.ZenRegister;
-import com.blamejared.crafttweaker.api.item.IIngredient;
+import com.blamejared.crafttweaker.api.*;
+import com.blamejared.crafttweaker.api.action.recipe.ActionAddRecipe;
+import com.blamejared.crafttweaker.api.annotation.ZenRegister;
+import com.blamejared.crafttweaker.api.ingredient.IIngredient;
 import com.blamejared.crafttweaker.api.item.IItemStack;
-import com.blamejared.iceandfiretweaker.action.ActionAddRecipe;
-import com.blamejared.iceandfiretweaker.action.ActionRemoveRecipe;
-import com.blamejared.iceandfiretweaker.action.ActionRemoveRecipeFromAll;
+import com.blamejared.crafttweaker.api.recipe.manager.base.IRecipeManager;
+import com.blamejared.crafttweaker_annotations.annotations.Document;
 import com.github.alexthe666.iceandfire.entity.DragonType;
-import com.github.alexthe666.iceandfire.recipe.DragonForgeRecipe;
+import com.github.alexthe666.iceandfire.recipe.*;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.RecipeType;
 import org.openzen.zencode.java.ZenCodeType;
 
+/**
+ * @docParam this <recipetype:iceandfire:dragonforge>
+ */
 @ZenRegister
-@ZenCodeType.Name("mods.iceandfire.DragonForge")
-public class DragonForge {
+@ZenCodeType.Name("mods.iceandfire.recipe.DragonForgeRecipeManager")
+@Document("mods/IceAndFire/recipe/manager/DragonForgeRecipeManager")
+public class DragonForge implements IRecipeManager<DragonForgeRecipe> {
     
+    /**
+     * Adds a new Fire Dragon Forge recipe.
+     *
+     * @param name     The name of the recipe
+     * @param output   The output item
+     * @param input    The input Ingredient
+     * @param blood    The blood Ingredient
+     * @param cookTime The cook time of the recipe
+     *
+     * @docParam name "fire_test"
+     * @docParam output <item:minecraft:diamond>
+     * @docParam input <item:minecraft:dirt>
+     * @docParam blood <item:minecraft:apple>
+     * @docParam cookTime 200
+     */
     @ZenCodeType.Method
-    public static void addFireRecipe(IItemStack output, IIngredient input, IIngredient blood) {
-        addRecipe(DragonType.FIRE.getName(), output, input, blood);
+    public void addFireRecipe(String name, IItemStack output, IIngredient input, IIngredient blood, int cookTime) {
+        addRecipe(name, DragonType.FIRE.getName(), output, input, blood, cookTime);
     }
     
+    /**
+     * Adds a new Ice Dragon Forge recipe.
+     *
+     * @param name     The name of the recipe
+     * @param output   The output item
+     * @param input    The input Ingredient
+     * @param blood    The blood Ingredient
+     * @param cookTime The cook time of the recipe
+     *
+     * @docParam name "ice_test"
+     * @docParam output <item:minecraft:diamond>
+     * @docParam input <item:minecraft:dirt>
+     * @docParam blood <item:minecraft:apple>
+     * @docParam cookTime 200
+     */
     @ZenCodeType.Method
-    public static void addIceRecipe(IItemStack output, IIngredient input, IIngredient blood) {
-        addRecipe(DragonType.ICE.getName(), output, input, blood);
+    public void addIceRecipe(String name, IItemStack output, IIngredient input, IIngredient blood, int cookTime) {
+        addRecipe(name, DragonType.ICE.getName(), output, input, blood, cookTime);
     }
     
+    /**
+     * Adds a new Lightning Dragon Forge recipe.
+     *
+     * @param name     The name of the recipe
+     * @param output   The output item
+     * @param input    The input Ingredient
+     * @param blood    The blood Ingredient
+     * @param cookTime The cook time of the recipe
+     *
+     * @docParam name "lightning_test"
+     * @docParam output <item:minecraft:diamond>
+     * @docParam input <item:minecraft:dirt>
+     * @docParam blood <item:minecraft:apple>
+     * @docParam cookTime 200
+     */
     @ZenCodeType.Method
-    public static void addLightningRecipe(IItemStack output, IIngredient input, IIngredient blood) {
-        addRecipe(DragonType.LIGHTNING.getName(), output, input, blood);
+    public void addLightningRecipe(String name, IItemStack output, IIngredient input, IIngredient blood, int cookTime) {
+        addRecipe(name, DragonType.LIGHTNING.getName(), output, input, blood, cookTime);
     }
     
-    @ZenCodeType.Method
-    public static void addRecipe(String dragonType, IItemStack output, IIngredient input, IIngredient blood) {
-        CraftTweakerAPI.apply(new ActionAddRecipe(new DragonForgeRecipe(input.asVanillaIngredient(), blood.asVanillaIngredient(), output.getInternal(), dragonType)));
+    public void addRecipe(String name, String dragonType, IItemStack output, IIngredient input, IIngredient blood, int cookTime) {
+        ResourceLocation id = CraftTweakerConstants.rl(fixRecipeName(name));
+        CraftTweakerAPI.apply(new ActionAddRecipe<>(this, new DragonForgeRecipe(id, input.asVanillaIngredient(), blood.asVanillaIngredient(), output.getInternal(), dragonType, cookTime)));
     }
     
-    @ZenCodeType.Method
-    public static void removeFireRecipe(IIngredient output) {
-        removeRecipe(DragonType.FIRE.getName(), output);
+    @Override
+    public RecipeType<DragonForgeRecipe> getRecipeType() {
+        return IafRecipeRegistry.DRAGON_FORGE_TYPE;
     }
-    
-    @ZenCodeType.Method
-    public static void removeIceRecipe(IIngredient output) {
-        removeRecipe(DragonType.ICE.getName(), output);
-    }
-    
-    @ZenCodeType.Method
-    public static void removeLightningRecipe(IIngredient output) {
-        removeRecipe(DragonType.LIGHTNING.getName(), output);
-    }
-    
-    @ZenCodeType.Method
-    public static void removeRecipe(String dragonType, IIngredient output) {
-        CraftTweakerAPI.apply(new ActionRemoveRecipe(dragonType, output));
-    }
-    
-    @ZenCodeType.Method
-    public static void removeRecipe(IIngredient output) {
-        CraftTweakerAPI.apply(new ActionRemoveRecipeFromAll(output));
-    }
-    
 }
